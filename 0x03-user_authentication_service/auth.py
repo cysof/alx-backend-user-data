82054143cfb3ac29cd1f269e140b87d56a82668a
cp4reallife@yahoo.com <cp4reallife@yahoo.com>
2024-07-17 04:21:48 +0100

    task 0 to task 12

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+"""defining an authentication method"""
+
+import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
+import bcrypt
+from uuid import uuid4
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using the bcrypt algorithm.
+    Arguments:
+      password(str) -- a string to be hashed
+    Returns:
+      A byte string.
+    """
+    hashed_pwd = password.encode('utf-8')  # Convert the password to bytes
+    # Generate a salt and hash the password
+    salted_hash = bcrypt.hashpw(hashed_pwd, bcrypt.gensalt())
+    return salted_hash
+
+def _generate_uuid() -> str:
+    """ _generate_uuid."""
+
+    return str(uuid4())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """
+        Initializes the Auth class.
+
+        Creates a new instance of the Auth class.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        self._db = DB()
+
+
+    
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a new user in the database.
+
+        Creates a new user object and adds it to the database.
+
+        Parameters:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            User: The new user object. Returns None if an error occurs.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates a user's login credentials.
+
+        Parameters:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            bool: True if the user is valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            return False
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Creates a new session for a user.
+
+        Parameters:
+            email (str): The email of the user.
+
+        Returns:
+            str: The session ID.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+        
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Retrieves a user from a session ID.
+
+        Parameters:
+            session_id (str): The session ID.
+
+        Returns:
+            User: The user object.
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+from typing import TypeVar
+
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id',
+                'reset_token']
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """find user_by."""
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
+        
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """update user."""
+        session = self._session
+        user = session.query(User).get(user_id)
+        if not user:
+            raise NoResultFound
+        for key, value in kwargs.items():
+            if key not in VALID_FIELDS:
+                raise ValueError
+            setattr(user, key, value)
+        session.commit()
